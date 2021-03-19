feb26274b1839d56ed2d787e4dd78821a35d7d71
Richard Ebeling <He3lixxx@users.noreply.github.com>
2021-03-19 14:43:58 +0100

    Update hls.js

--- utils/build-hls-js.sh
+++ utils/build-hls-js.sh
@@ -1,14 +1,20 @@
 # Hls.js is shipped as UMD modules that may cause compatibility issues if the surrounding website uses a modules loader, such as require.js
 # Therefore, hls.js is rebuilt with output library target window to ensure it is globally available.
+# See https://github.com/video-dev/hls.js/issues/2910 on status of an ES6 module build of hls.js
 echo 'Rebuilding hls.js...'
 cd node_modules/hls.js
 npm install --legacy-peer-deps --silent
-node_modules/.bin/webpack --config-name debug --output-library-target window --display errors-only
-#node_modules/.bin/webpack --config-name dist --output-library-target window --display errors-only
-node_modules/.bin/webpack --config-name light --output-library-target window --display errors-only
-#node_modules/.bin/webpack --config-name light-dist --output-library-target window --display errors-only
+
+# For some reason, they decided to not include their build config in their npm packages anymore
+# see https://github.com/video-dev/hls.js/commit/083322b49e3c09ae35a33e553fe79343cf9e68dd
+wget -nc https://raw.githubusercontent.com/video-dev/hls.js/v0.14.17/webpack.config.js
+
+npx webpack --config-name debug --output-library-target window --display errors-only
+npx webpack --config-name light --output-library-target window --display errors-only
+
 tail -n +2 dist/hls.js > temp
 cat temp > dist/hls.js
 tail -n +2 dist/hls.light.js > temp
 cat temp > dist/hls.light.js
+
 rm temp
